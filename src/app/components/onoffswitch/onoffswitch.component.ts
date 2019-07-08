cc4c52429b5d094821ad2c47502138c24deee80f
aoweeks <aoweeks@gmail.com>
2019-07-08 19:48:55 +0100

    Generate OnOffSwitch component

---
+++ src/app/components/onoffswitch/onoffswitch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onoffswitch',
+  templateUrl: './onoffswitch.component.html',
+  styleUrls: ['./onoffswitch.component.scss']
+})
+export class OnoffswitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
