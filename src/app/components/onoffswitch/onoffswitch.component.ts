97fff9d160cdc89a1a4715ed5071408293478951
aoweeks <aoweeks@gmail.com>
2019-07-09 17:52:50 +0100

    Add OnOffSwitch svg and functions

--- src/app/components/onoffswitch/onoffswitch.component.ts
+++ src/app/components/onoffswitch/onoffswitch.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-onoffswitch',
+  selector: 'on-off-switch',
   templateUrl: './onoffswitch.component.html',
   styleUrls: ['./onoffswitch.component.scss']
 })
 export class OnoffswitchComponent implements OnInit {
 
+  private on: Boolean = true;
   constructor() { }
 
   ngOnInit() {
   }
 
+  clicked() {
+    this.on = !this.on;
+    console.log(this.on);
+  }
 }
